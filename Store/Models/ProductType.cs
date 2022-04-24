1c130eb05e250140700d1f45999b751d50bedd5f
Alesger <Alasgarma@code.edu.az>
2022-04-24 14:46:01 +0400

    Task

---
+++ Store/Models/ProductType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Models
+{
+    enum ProductType
+    {
+        //Bakery=1,
+        //Drink,
+        //Meat,
+        //Fish,
+    }
+}
---
+++ Store/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Store.Models;
+
+namespace Store
+{
+    class Program
+    {
+        public static string B1000 { get; private set; }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Product.Democlass();
+            //Product.TotalCount();
+
+            //Product product = new Product();
+
+
+            #region EnumTest
+            //Productstype productstype = new Productstype();
+            //Console.WriteLine("Siyahidan Id Daxil Et");
+            //foreach (var item in Enum.GetValues(typeof(Productstype)))
+            //{
+            //    Console.WriteLine($"Id: {(int)item} Name: {item}");
+            //}
+            //string a = Console.ReadLine();
+            //int.TryParse(a, out int b);
+            //Console.WriteLine($"Secdiyiniz Mehsul:{(Productstype)b}");
+            #endregion
+            #region Task
+            //int discount = 10;
+            //double DiscountPrice, AfterPrice;
+            //double beforePrice = 4;
+            //int TotalCount = 0;
+
+            //DiscountPrice = beforePrice * discount / 100;
+            //AfterPrice = beforePrice - DiscountPrice;
+            //Console.Write("Endirim sonrası qiymeti: " + AfterPrice);
+            //Console.WriteLine("\n-------------------------------------");
+
+
+            ////Console.WriteLine("Value"+ Product.Democlass());
+            //Console.WriteLine("Classda Yaratdiginiz umumi obyektlerin sayi");
+            //Console.WriteLine( TotalCount + Product.Sum());
+            #endregion
+        }
+    }
+}
